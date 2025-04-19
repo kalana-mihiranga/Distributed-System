2ef3856874c486bb92a3a4b75100a497ad145c4d
Kalana Mihiranga <kalanamihiranga97@gmail.com>
2025-04-19 23:17:27 +0530

    first commit

---
+++ src/test/java/com/esad/dc/DcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esad.dc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
